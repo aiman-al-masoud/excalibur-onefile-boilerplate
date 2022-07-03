d8b2220eb2423a79c089b815f857efde42f52e32
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-03 09:56:37 +0200

    ...

--- app/src/Samurai.ts
+++ app/src/Samurai.ts
@@ -11,25 +11,27 @@ export class Samurai extends ex.Actor {
             collisionType: ex.CollisionType.Active,
             collisionGroup: ex.CollisionGroupManager.groupByName("Samurai"),
             collider: ex.Shape.Box(32, 50, ex.Vector.Half, ex.vec(0, 3)),
-            color : ex.Color.Black
+            color: ex.Color.Black
         });
     }
 
 
     onInitialize(engine: ex.Engine) {
-        
-        //run 
-        const run = ex.Animation.fromSpriteSheet(samuraiRunSpriteSheet, [0, 1, 2,3,4,5,6,7], 150);
+
+        // run to right
+        const run = ex.Animation.fromSpriteSheet(samuraiRunSpriteSheet, [0, 1, 2, 3, 4, 5, 6, 7], 150);
         run.scale = new ex.Vector(2, 2);
-        this.graphics.add("run", run)
+        this.graphics.add("runToRight", run)
+
+        // run to left
+        const runToLeft = run.clone()
+        runToLeft.flipHorizontal = true
+        this.graphics.add("runToLeft", runToLeft)
 
         // idle
-        const idle = ex.Animation.fromSpriteSheet(samuraiIdleSpriteSheet, [...new Array(samuraiIdleSpriteSheet.columns).keys()] , 150)
-        idle.scale = new ex.Vector(2,2)
+        const idle = ex.Animation.fromSpriteSheet(samuraiIdleSpriteSheet, [...new Array(samuraiIdleSpriteSheet.columns).keys()], 150)
+        idle.scale = new ex.Vector(2, 2)
         this.graphics.add("idle", idle)
-
-
-
 
 
     }
@@ -39,18 +41,32 @@ export class Samurai extends ex.Actor {
         this.vel.x = 0;
 
         // Player input
-        if(engine.input.keyboard.isHeld(ex.Input.Keys.Left)) {
+        if (engine.input.keyboard.isHeld(ex.Input.Keys.Left)) {
             this.vel.x = -150;
         }
 
-         // Player input
-         if(engine.input.keyboard.isHeld(ex.Input.Keys.Right)) {
+        // Player input
+        if (engine.input.keyboard.isHeld(ex.Input.Keys.Right)) {
             this.vel.x = 150;
         }
+
+
+        /**
+         * change animation based on velocity
+         */
 
         if (this.vel.x === 0) {
             this.graphics.use("idle")
         }
+
+        if (this.vel.x < 0) {
+            this.graphics.use("runToLeft")
+        }
+
+        if (this.vel.x > 0) {
+            this.graphics.use("runToRight")
+        }
+
     }
 
 
