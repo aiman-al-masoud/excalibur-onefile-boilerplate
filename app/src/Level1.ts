3430f48d6a2ee88f9fb682f3420ef0f17b324d6d
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-04 18:50:27 +0200

    ...

--- app/src/Level1.ts
+++ app/src/Level1.ts
@@ -2,7 +2,6 @@ import {Engine, Scene} from 'excalibur'
 import * as ex from 'excalibur'
 import { Samurai } from './Samurai';
 import { Floor } from './Floor';
-import Bomb from './Bomb';
 
 export default class Level1 extends Scene{
 
@@ -24,15 +23,7 @@ export default class Level1 extends Scene{
         engine.add(a)
 
         const floor = new Floor(0, 300, 15, 1);
-        engine.add(floor)
-
-
-        engine.add(new Bomb({x: engine.halfDrawWidth + 50, 
-            y: engine.halfDrawHeight - 100,
-            xVel: 0, 
-            yVel :  -500} ))
-
-        
+        engine.add(floor)        
       
         this.camera.clearAllStrategies();
         this.camera.strategy.elasticToActor(a, 0.05, 0.1);
